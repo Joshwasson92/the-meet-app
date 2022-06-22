032ff988860c678102d97d7b6ab785865d9ea266
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-06-21 21:15:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./nprogress.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
+import { getEvents, checkToken } from "./api";
 
 class App extends Component {
   state = {
@@ -12,13 +12,20 @@ class App extends Component {
     locations: [],
     numberOfEvents: [30],
   };
-  componentDidMount() {
+  async componentDidMount() {
+    const accessToken = localStorage.getItem("access_token");
+    const validToken =
+      accessToken !== null ? await checkToken(accessToken) : false;
+    this.setState({ tokenCheck: validToken });
+    if (validToken === true) this.updateEvents();
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    if (code && this.mounted === true && validToken === false) {
+      this.setState({ tokenCheck: true });
+      this.updateEvents();
+    }
   }
 
   componentWillUnmount() {
